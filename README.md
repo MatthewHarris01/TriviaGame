cf4d404b5205a68680aea667a1bdff81f66f535d
Matthew Harris <simonh241@yahoo.com>
2019-12-30 19:34:07 -0800

    Version 0.3 see Readme.md

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ play a timed trivia game with the computer.
 Version 0.0 -- no html or code created, all .html and .js and .css files are empty.
 Version 0.1 -- file placeholders created
 Version 0.2 -- favicon file added.
+Version 0.3 -- app.js file added. .css file partially populated. Images added.
---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// this is the game code for Space Exploratin Trivia game
+
+console.log("this is the .js include file");
+
+/* only do work if the document is ready */
+$(document).ready(function () {
+  console.log("inside document is ready function");
+
+
+  console.log("end of document ready function");
+} //end of document ready function
+) //end of document ready evant handler parameter list
